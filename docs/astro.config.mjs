be6a1882c305c3c60a641633f205f098d68cdee1
Navneet Nagpal <navneetnaagpal@gmail.com>
2025-03-12 09:53:21 +0530

    Fixed build

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,28 +1,43 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+// import catppuccin from 'starlight-theme-catppuccin';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  build: {
+    inlineStylesheets: 'never',
+  },
+  integrations: [
+    starlight({
+      title: 'My Docs',
+      social: {
+        github: 'https://github.com/qubejs/qubejs',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
+
+  vite: {
+    css: {
+      transformer: "lightningcss",
+    },
+    plugins: [
+      tailwindcss(),
+      // catppuccin({ dark: 'macchiato-sky', light: 'latte-sky' }),
+    ],
+  },
 });
